91693c045e3bc155a7e86b8f0fc4eee46eb7e8a5
Al Arafat Siddique <alarafatsiddique@gmail.com>
2023-06-20 14:41:31 +0600

    refresh order after deletion

--- pages/account/orders.js
+++ pages/account/orders.js
@@ -77,6 +77,7 @@ const Orders = () => {
           progress: undefined,
           theme: "dark",
         });
+        getOrders();
       }
     }catch(error){
       // console.log(error)
@@ -93,7 +94,7 @@ const Orders = () => {
   }
   const orderDeleteHandler=(id)=>{
     deleteOrder(id);
-    getOrders();
+    
   }
 
   useEffect(() => {
