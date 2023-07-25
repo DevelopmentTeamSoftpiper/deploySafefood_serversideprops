6fd6ee3032c315d04e56d5b408da2669f1d56e78
msajiba <asajib499@gmail.com>
2023-07-25 12:40:20 +0600

    301 url, social button, backlinks added for seo & remove ridirect

--- next.config.js
+++ next.config.js
@@ -4,20 +4,7 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: 'http://safefoods.com.bd/',
-        destination: 'https://safefoods.com.bd/',
-        permanent: true,
-      },
-      {
-        source: 'http://www.safefoods.com.bd/',
-        destination: 'https://www.safefoods.com.bd/',
-        permanent: true,
-      },
-    ]
-  },
+  
 };
 
 module.exports = nextConfig;
