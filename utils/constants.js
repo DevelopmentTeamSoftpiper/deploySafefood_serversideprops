a75c89b1cc5028eea37cd1220c3855f48c231472
msajiba <asajib499@gmail.com>
2024-05-13 08:08:10 +0600

    Ip changed

--- utils/constants.js
+++ utils/constants.js
@@ -3,4 +3,4 @@ export const RATE_LIMIT_TIME = 60 * 1000 * 5;
 export const RATE_LIMIT_TIME_MIN = 5;
 
 export const IP_ADDRESS_URL =
-  "https://ftpdemo.softpiper.com/getUserPrivateIpAddress";
+  "https://api.ipify.org";
